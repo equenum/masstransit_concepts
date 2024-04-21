d58e2ca511a331def8ccb851ca07f0a67ba1db83
Egor Quenum <georgequenum@yahoo.com>
2024-04-21 23:42:05 +0400

    Add hosting to DeliveryService

--- DeliveryService/Program.cs
+++ DeliveryService/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+
+namespace OrderService.Api;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => 
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) => {});
+}
