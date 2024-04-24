0e856cc9df3e29c739761e60623d79a491461e2b
Egor Quenum <georgequenum@yahoo.com>
2024-04-25 00:27:38 +0400

    Add OrdersController

---
+++ OrderService.Api/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class OrdersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(Database.Orders);
+    }
+}
