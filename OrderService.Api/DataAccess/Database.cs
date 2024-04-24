d1bddf2b4c6acd9b99eeb6a5faa13c2bdbff3187
Egor Quenum <georgequenum@yahoo.com>
2024-04-25 00:27:04 +0400

    Add fake database

---
+++ OrderService.Api/DataAccess/Database.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace OrderService.Api;
+
+public static class Database
+{
+    public static readonly List<Order> Orders = new List<Order>()
+    {
+        new Order()
+        {
+            Id = 1
+        },
+        new Order() 
+        { 
+            Id = 2
+        },
+        new Order()
+        {
+            Id = 3
+        }
+    };
+}
---
+++ OrderService.Api/Models/Order.cs
@@ -0,0 +1,6 @@
+﻿namespace OrderService.Api;
+
+public class Order
+{
+    public int Id { get; set; }
+}
