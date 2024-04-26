22473ab1d2e38dac2bcdf0c8ca1b4a0b1c1f8f05
Egor Quenum <georgequenum@yahoo.com>
2024-04-26 23:33:31 +0400

    Implement OrdersController

--- OrderService.Api/Controllers/OrdersController.cs
+++ OrderService.Api/Controllers/OrdersController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Api.Models;
+using OrderService.Api.Models.Requests;
 
-namespace OrderService.Api;
+namespace OrderService.Api.Controllers;
 
 [ApiController]
 [Route("api/public/[controller]")]
@@ -10,5 +13,35 @@ public class OrdersController : ControllerBase
     public IActionResult Get()
     {
         return Ok(Database.Orders);
+    }
+
+    [HttpGet("{id:int}", Name = "GetById")]
+    public IActionResult Get(int id)
+    {
+        var order = Database.Orders.FirstOrDefault(o => o.Id == id);
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(order);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateOrderRequest request)
+    {
+        var latestId = Database.Orders.Max(order => order.Id);
+        var order = new Order()
+        {
+            Id = latestId + 1,
+            CustomerId = request.CustomerId,
+            Type = request.Type,
+            DeliveryDetails = request.DeliveryDetails,
+            Products = request.Products
+        };
+
+        Database.Orders.Add(order);
+
+        return CreatedAtRoute("GetById", new { id = order.Id }, order);
     }
 }
--- OrderService.Api/DataAccess/Database.cs
+++ OrderService.Api/DataAccess/Database.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using OrderService.Api.Models;
 
 namespace OrderService.Api;
 
@@ -6,6 +7,7 @@ public static class Database
 {
     public static readonly List<Order> Orders = new List<Order>()
     {
+        // todo properly populate the models
         new Order()
         {
             Id = 1
