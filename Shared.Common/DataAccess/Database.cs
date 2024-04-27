f55353bd4c87113c4af38e6cf0cf51a3a314d701
Egor Quenum <georgequenum@yahoo.com>
2024-04-28 01:50:33 +0400

    Finalized the db mock

--- Shared.Common/DataAccess/Database.cs
+++ Shared.Common/DataAccess/Database.cs
@@ -5,20 +5,5 @@ namespace Shared.Common.DataAccess;
 
 public static class Database
 {
-    public static readonly List<Order> Orders = new List<Order>()
-    {
-        // todo properly populate the models
-        new Order()
-        {
-            Id = 1
-        },
-        new Order() 
-        { 
-            Id = 2
-        },
-        new Order()
-        {
-            Id = 3
-        }
-    };
+    public static readonly List<Order> Orders = new List<Order>();
 }
